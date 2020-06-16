1ee7711da34daee7afe91cdff8bbd8033b27d8ee
Mike Wendt <mike.wendt@me.com>
2020-06-16 00:20:10 -0400

    FIX Update build script to copy multiple libs in 8.0 release

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,11 @@ mkdir -p $PREFIX/include
 cp include/cudnn.h $PREFIX/include/
 
 mkdir -p $PREFIX/lib
-cp lib64/libcudnn.so.8.0.0 $PREFIX/lib/
-ln -s $PREFIX/lib/libcudnn.so.8.0.0 $PREFIX/lib/libcudnn.so.8
-ln -s $PREFIX/lib/libcudnn.so.8 $PREFIX/lib/libcudnn.so
+cp lib64/libcudnn*.so.8.0.0 $PREFIX/lib/
+LIBS=`ls $PREFIX/lib/`
+for LIB in $LIBS ; do
+  LIB_NAME=`echo $LIB | tr '.' ' ' | awk '{ print $1 }'`
+  echo "creating symlinks for $LIB_NAME"
+  ln -s $PREFIX/lib/$LIB_NAME.so.8.0.0 $PREFIX/lib/$LIB_NAME.so.8
+  ln -s $PREFIX/lib/$LIB_NAME.so.8 $PREFIX/lib/$LIB_NAME.so
+done

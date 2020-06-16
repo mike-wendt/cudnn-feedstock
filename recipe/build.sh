533608b29bfbddc2643c1211ed214d1ed8801cd4
Mike Wendt <mike.wendt@me.com>
2020-06-15 22:53:40 -0400

    ENH Update version for cuDNN 8

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,6 @@ mkdir -p $PREFIX/include
 cp include/cudnn.h $PREFIX/include/
 
 mkdir -p $PREFIX/lib
-cp lib64/libcudnn.so.7.6.5 $PREFIX/lib/
-ln -s $PREFIX/lib/libcudnn.so.7.6.5 $PREFIX/lib/libcudnn.so.7
-ln -s $PREFIX/lib/libcudnn.so.7 $PREFIX/lib/libcudnn.so
+cp lib64/libcudnn.so.8.0.0 $PREFIX/lib/
+ln -s $PREFIX/lib/libcudnn.so.8.0.0 $PREFIX/lib/libcudnn.so.8
+ln -s $PREFIX/lib/libcudnn.so.8 $PREFIX/lib/libcudnn.so

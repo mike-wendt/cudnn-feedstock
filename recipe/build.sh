15efaa1770742e464b15394093567a332f601dfc
Mike Wendt <mike.wendt@me.com>
2020-10-21 15:17:20 -0400

    FIX Update for version 8.0.4

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,11 @@ mkdir -p $PREFIX/include
 cp include/cudnn*.h $PREFIX/include/
 
 mkdir -p $PREFIX/lib
-cp lib64/libcudnn*.so.8.0.0 $PREFIX/lib/
+cp lib64/libcudnn*.so.8.0.4 $PREFIX/lib/
 LIBS=`ls $PREFIX/lib/`
 for LIB in $LIBS ; do
   LIB_NAME=`echo $LIB | tr '.' ' ' | awk '{ print $1 }'`
   echo "creating symlinks for $LIB_NAME"
-  ln -s $PREFIX/lib/$LIB_NAME.so.8.0.0 $PREFIX/lib/$LIB_NAME.so.8
+  ln -s $PREFIX/lib/$LIB_NAME.so.8.0.4 $PREFIX/lib/$LIB_NAME.so.8
   ln -s $PREFIX/lib/$LIB_NAME.so.8 $PREFIX/lib/$LIB_NAME.so
 done
